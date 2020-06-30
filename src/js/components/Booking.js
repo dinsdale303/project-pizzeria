01fbaa20fea3fbbd97805e465cfc75e3d4b28bbf
Piotr Stefanowicz <piotrekstefanowicz@gmail.com>
2020-06-30 08:48:02 +0200

    add DatePicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,7 @@
 import { select, templates } from '../settings.js';
 import { utils } from '../utils.js';
 import AmountWidget from './AmountWidget.js';
-
+import DatePicker from './DatePicker.js';
 
 class Booking {
   constructor(element) {
@@ -26,6 +26,9 @@ class Booking {
     );
     thisBooking.dom.hoursAmount = thisBooking.generatedDOM.querySelector(
       select.booking.hoursAmount
+    );  
+    thisBooking.dom.datePicker = thisBooking.generatedDOM.querySelector(
+      select.widgets.datePicker.wrapper
     );
     thisBooking.dom.wrapper.appendChild(thisBooking.generatedDOM);
   }
@@ -35,6 +38,7 @@ class Booking {
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   
   }
 }
